c694c8e49f47ebd12b367a256c40ee9a2ed0df63
VattraF4 <vattraramo13@gmail.com>
2024-12-05 14:04:25 +0700

    update dynamic to github file

--- assets/js/member-dynamic.js
+++ assets/js/member-dynamic.js
@@ -1,15 +1,20 @@
+const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+const url = 'https://raw.githubusercontent.com/VattraF4/Guidelines-FTB-Bank/main/Members/our-team.html';
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('../Members/our-team.html')
-        .then(response => {
-            console.log('Response:', response);
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('team-container').innerHTML = data;
-            console.log('Team section loaded successfully.');
-        })
-        .catch(error => console.error('Error loading team section:', error));
+    fetch('https://raw.githubusercontent.com/VattraF4/Guidelines-FTB-Bank/main/Members/our-team.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('team-container').innerHTML = data;
+        console.log('Team section loaded successfully.');
+    })
+    .catch(error => console.error('Error loading team section:', error));
 });
+
+
