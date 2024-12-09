41db9e35419a8a2d92b673f1fc9b487ceb822eba
VattraF4 <vattraramo13@gmail.com>
2024-12-09 13:37:20 +0700

    fix all member to display

--- assets/js/callAllMember.js
+++ assets/js/callAllMember.js
@@ -1,3 +1,5 @@
+//github display html file by dynamically loading
+// Call Ra-Vattra from github.com
 document.addEventListener("DOMContentLoaded", () => {
     fetch('/Guidelines-FTB-Bank/Members/Ra Vattra.html') // Adjust the path for GitHub Pages
     // fetch('../Members/Ra Vattra.html') // Adjust the path for GitHub Pages
@@ -13,7 +15,127 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(error => console.error('Error loading team section:', error));
 });
+// Yan Yan-Pich from gitServer
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/Guidelines-FTB-Bank/Members/Yan Pich.html') // Adjust the path for GitHub Pages
+    // fetch('../Members/Ra Vattra.html') // Adjust the path for GitHub Pages
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('mid-pich-yan').innerHTML = data;
+        console.log('Team section loaded successfully.');
+    })
+    .catch(error => console.error('Error loading team section:', error));
+});
 
+// Ra Raksa from gitServer
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/Guidelines-FTB-Bank/Members/Ra Raksa.html') // Adjust the path for GitHub Pages
+    // fetch('../Members/Ra Vattra.html') // Adjust the path for GitHub Pages
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('mid-ra-raksa').innerHTML = data;
+        console.log('Team section loaded successfully.');
+    })
+    .catch(error => console.error('Error loading team section:', error));
+});
+
+// RC Nan from gitServer
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/Guidelines-FTB-Bank/Members/RC Nan.html') // Adjust the path for GitHub Pages
+    // fetch('../Members/Ra Vattra.html') // Adjust the path for GitHub Pages
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('mid-rc-nan').innerHTML = data;
+        console.log('Team section loaded successfully.');
+    })
+    .catch(error => console.error('Error loading team section:', error));
+});
+
+// Ralin from gitServer
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/Guidelines-FTB-Bank/Members/Ralin Thai.html') // Adjust the path for GitHub Pages
+    // fetch('../Members/Ra Vattra.html') // Adjust the path for GitHub Pages
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('mid-ralin').innerHTML = data;
+        console.log('Team section loaded successfully.');
+    })
+    .catch(error => console.error('Error loading team section:', error));
+});
+
+// Run Oudom from gitServer
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/Guidelines-FTB-Bank/Members/Run Oudom.html') // Adjust the path for GitHub Pages
+    // fetch('../Members/Ra Vattra.html') // Adjust the path for GitHub Pages
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('mid-run-oudom').innerHTML = data;
+        console.log('Team section loaded successfully.');
+    })
+    .catch(error => console.error('Error loading team section:', error));
+});
+
+// Chamnan David from gitServer
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/Guidelines-FTB-Bank/Members/Chamnan David.html') // Adjust the path for GitHub Pages
+    // fetch('../Members/Ra Vattra.html') // Adjust the path for GitHub Pages
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('mid-chamnan-david').innerHTML = data;
+        console.log('Team section loaded successfully.');
+    })
+    .catch(error => console.error('Error loading team section:', error));
+});
+
+// Yorn Soyun from gitServer
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/Guidelines-FTB-Bank/Members/Yorn soyun.html') // Adjust the path for GitHub Pages
+    // fetch('../Members/Ra Vattra.html') // Adjust the path for GitHub Pages
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('mid-yorn-soyun').innerHTML = data;
+        console.log('Team section loaded successfully.');
+    })
+    .catch(error => console.error('Error loading team section:', error));
+});
+
+// ------------- Call file with local links for local files host -----------------//
+// Call Ra Vattra
 document.addEventListener("DOMContentLoaded", () => {
     fetch('../Members/Ra Vattra.html')
     .then(response => response.ok ? response.text() : Promise.reject(`HTTP error! Status: ${response.status}`))
