5b81f680df8f054be98c6462324a5f4a82bb426f
VattraF4 <vattraramo13@gmail.com>
2024-12-09 12:21:47 +0700

    try to change path to fix diaplay member

--- assets/js/callAllMember.js
+++ assets/js/callAllMember.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('../Members/Ra Vattra.html')
+    fetch('https://github.com/VattraF4/Guidelines-FTB-Bank/blob/main/Members/Ra%20Vattra.html')
+    // fetch('../Members/Ra Vattra.html')
     .then(response => response.ok ? response.text() : Promise.reject(`HTTP error! Status: ${response.status}`))
     .then(data => {
         document.getElementById('mid-vattra').innerHTML = data;
