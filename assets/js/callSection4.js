a86ac5a035801cdfff808d651012f0b65cb98e49
VattraF4 <vattraramo13@gmail.com>
2024-12-11 22:34:46 +0700

    add section4 as dynamic

---
+++ assets/js/callSection4.js
@@ -0,0 +1,21 @@
+
+// Use this method to call files in project directory
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('./Section/4.Resource.html') // Update the path to the correct one
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(data => {
+      const cssCodeElement = document.getElementById('resource');
+      if (cssCodeElement) {
+        cssCodeElement.innerHTML = data;
+        console.log('Seciontion "Re loaded successfully.');
+      } else {
+        console.error('Element "resource" not found');
+      }
+    })
+    .catch(error => console.error('Error loading file', error));
+});
